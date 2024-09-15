080cbbda52c062d7210acbc457dc66fe41feb4bd
Văn Hoài <vanhoai1912@gmail.com>
2024-09-15 18:37:09 +0700

    model

---
+++ ToeicWeb/Models/Ma_bai_ngu_phap.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToeicWeb.Models
+{
+    public class Ma_bai_ngu_phap
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Ten_bai { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ ToeicWeb/Models/Ma_bai_tu_vung.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToeicWeb.Models
+{
+    public class Ma_bai_tu_vung
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Ten_bai {  get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
