fc3ce48d5d3c091d12136035314091d46d9a9daa
Văn Hoài <vanhoai1912@gmail.com>
2024-09-20 09:57:04 +0700

    .

--- ToeicWeb/Models/ViewModels/CauhoiBTngeVM.cs
+++ ToeicWeb/Models/ViewModels/CauhoiBTngeVM.cs
@@ -2,13 +2,14 @@
 {
     public class CauhoiBTngeVM
     {
-        public IFormFile ExcelFile { get; set; }
-        public string Tieu_de { get; set; }
-        public int Id { get; set; }
-        public int Part { get; set; }
-        public List<Ma_bai_tap_nge> Mabaitapnges { get; set; }
-        public Ma_bai_tap_nge Mabaitapnge { get; set; }
-        public List<IFormFile> ImageFile { get; set; }
-        public List<IFormFile> AudioFile { get; set; }
+            public IFormFile ExcelFile { get; set; }
+            public string Tieu_de { get; set; }
+            public int Id { get; set; }
+            public int Part { get; set; }
+            public List<Ma_bai_tap_nge> Mabaitapnges { get; set; }
+            public Ma_bai_tap_nge Mabaitapnge { get; set; }
+            public List<IFormFile> ImageFile { get; set; } = new List<IFormFile>();  // Khởi tạo để tránh null
+            public List<IFormFile> AudioFile { get; set; } = new List<IFormFile>();  // Khởi tạo để tránh null
+
     }
 }
