3fbd1a5bc44fa661ba88127e3e9d931a5cc7a44a
Văn Hoài <vanhoai1912@gmail.com>
2024-10-03 21:40:54 +0700

    .

---
+++ ToeicWeb/Areas/Admin/Controllers/BThiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Toeic.DataAccess;
+
+namespace ToeicWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class BThiController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _environment;
+
+        public BThiController(UserManager<IdentityUser> userManager, ApplicationDbContext db, RoleManager<IdentityRole> roleManager, IWebHostEnvironment environment)
+        {
+            _db = db;
+            _environment = environment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
